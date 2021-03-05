e01a34ea0dca254f87185cf1a77baaa5e3c56193
Samuel Vera <samuelvera1999@gmail.com>
2021-03-05 13:22:13 -0400

    Fetching data from a component lesson

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -2,12 +2,34 @@
   <h1>Job Details Page</h1>
   <!--Can be done using the route object to extract params with databinding ($route.params.id)-->
   <p>The job id is: {{ id }}</p>
+  <p v-if="job === null">Loading...</p>
+  <div v-else>
+    <h2>{{ job.title }}</h2>
+    <h3>{{ job.details }}</h3>
+  </div>
 </template>
 
 <script>
 export default {
   //Accepting route params as props
   props: ["id"],
+  //Data
+  data() {
+    return {
+      job: null,
+    };
+  },
+  //On mounted to DOM
+  mounted() {
+    //Get a single job
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      //Track response and transform from JSON to data
+      .then((res) => res.json())
+      //Get data as JS
+      .then((data) => (this.job = data))
+      //Catch the errors and log them
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -12,12 +12,21 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "UX Designer", id: 1, details: "lorem" },
-        { title: "Web Developer", id: 2, details: "lorem" },
-        { title: "Vue Developer", id: 3, details: "lorem" },
-      ],
+      //Initial jobs property (will be filled with external data)
+      jobs: [],
     };
+  },
+  //When the component has been mounted to the DOM
+  mounted() {
+    //Good moment on the lifecycle to fetch from API
+    //Fetch the data from the end point
+    fetch("http://localhost:3000/jobs")
+      //Track response and transform from JSON to data
+      .then((res) => res.json())
+      //Get data as JS
+      .then((data) => (this.jobs = data))
+      //Catch the errors and log them
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
